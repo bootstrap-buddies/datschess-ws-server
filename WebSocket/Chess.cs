cfc6a2c3e5e808a8cc9d41732c565b33880d0f19
imverbatim <ajdelco22@gmail.com>
2022-02-06 02:15:05 -0500

    first commit

---
+++ WebSocket/Chess.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+using System.Text.Json;
+
+namespace Example
+{
+
+    public class Message
+    {
+        public string sourceSquare { get; set; }
+        public string targetSquare { get; set; }
+
+    }
+    public class Chess : WebSocketBehavior
+    {
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Message message =
+                JsonSerializer.Deserialize<Message>(e.Data);
+
+            Console.WriteLine($"sourceSquare: {message.sourceSquare}");
+            Console.WriteLine($"targetSquare: {message.targetSquare}");
+
+            var outData = new Message
+            {
+                sourceSquare = "bruh",
+                targetSquare = "yo"
+            };
+
+            Send(JsonSerializer.Serialize(outData));
+
+        }
+        protected override void OnClose(CloseEventArgs e)
+        {
+        }
+
+        protected override void OnOpen()
+        {
+            var message = new Message
+            {
+                sourceSquare = "bruh",
+                targetSquare = "yo"
+            };
+            Console.WriteLine("Open");
+            Send(JsonSerializer.Serialize(message));
+        }
+    }
+}
---
+++ WebSocket/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+
+
+namespace Example
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var wssv = new WebSocketServer("ws://localhost:8000");
+            wssv.AddWebSocketService<Chess>("/Chess");
+            wssv.Start();
+            if (wssv.IsListening)
+            {
+                Console.WriteLine("Listening on port {0}, and providing WebSocket services:", wssv.Port);
+
+                foreach (var path in wssv.WebSocketServices.Paths)
+                    Console.WriteLine("- {0}", path);
+            }
+
+            Console.WriteLine("\nPress Enter key to stop the server...");
+            Console.ReadLine();
+
+            wssv.Stop();
+        }
+    }
+}
